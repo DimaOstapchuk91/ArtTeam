670d0b3618c40911c8e29e2cd5124214225a0e87
ReaperDal <reaper_dal@knu.ua>
2024-05-05 19:20:10 +0300

    add js to show more button

--- src/js/toggleItemsVisibility.js
+++ src/js/toggleItemsVisibility.js
@@ -6,16 +6,17 @@ function toggleItemsVisibility() {
     for (var i = listItems.length - 2; i < listItems.length; i++) {
       listItems[i].style.display = 'block';
     }
-    //  "Show less"
+    //"Show less"
     document.querySelector('.catalog-button').textContent = 'Show less';
   } else {
     // hide last 2
     for (var i = listItems.length - 2; i < listItems.length; i++) {
       listItems[i].style.display = 'none';
     }
-    //  "Show more"
+    //"Show more"
     document.querySelector('.catalog-button').textContent = 'Show more';
   }
   buttonClicked = !buttonClicked;
 }
+
 document.querySelector('.catalog-button').addEventListener('click', toggleItemsVisibility);
