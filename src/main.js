178c3da0d22f6982ed0cdbc5979c99669fb57b51
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-09 16:45:17 +0300

    add button swiper and add close backdrop and add scroll up

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import './js/menu.js';
 import './js/toggleItemsVisibility.js';
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 const swiper = new Swiper('.swiper-container', {
+  lazyPreloadPrevNext: 0,
+  grabCursor: true,
+  direction: 'horizontal',
   loop: true,
+  speed: 300,
+  autoplay: {
+    delay: 500,
+    disableOnInteraction: false,
+  },
   slidesPerView: 1,
   spaceBetween: 16,
   breakpoints: {
@@ -14,16 +24,21 @@ const swiper = new Swiper('.swiper-container', {
       spaceBetween: 16,
     },
     1280: {
-      loop: false,
       slidesPerView: 3,
       spaceBetween: 20,
+      loop: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
     },
   },
 
+  modules: [Navigation, Pagination],
   pagination: {
     el: '.pagination',
-    bulletClass: 'pagination__button',
-    bulletActiveClass: 'pagination__button--active',
+    bulletClass: 'pagination-button',
+    bulletActiveClass: 'pagination-button--active',
   },
 
   navigation: {
